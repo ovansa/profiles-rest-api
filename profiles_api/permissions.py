248457893bbd6a6dc1362c65824fb5c20c11d82b
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2020-05-09 16:07:30 +0100

    Added user profile feeds

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,12 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return object.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    '''Allows users update own status'''
+    def has_object_permission(self, request, view, object):
+        '''Check is trying to update own status'''
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return object.user_profile.id == request.user.id
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewset)
+router.register('feed', views.UserProfileFeedsViewset)
 
 urlpatterns = [
     path('hello/', views.HelloAPIView.as_view()),
